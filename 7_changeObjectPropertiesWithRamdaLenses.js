3a849ea5a3ac71b4e8acdb29c2874387e8d7c0f9
Nick Masters <master-7@inbox.ru>
2017-10-22 15:51:49 +0300

    7

---
+++ 7_changeObjectPropertiesWithRamdaLenses.js
@@ -0,0 +1,22 @@
+// We don't need require in plunker
+//const R = require('ramda')
+
+const person = {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+}
+
+// const fLens = R.lens(R.prop('firstName'), R.assoc('firstName'))
+const fLens = R.lensProp('firstName')
+
+// const result = R.view(fLens, person)
+// const result = R.set(fLens, 'Wilma', person)
+
+// const firstName = R.view(fLens, person)
+// const upperName = R.toUpper(firstName)
+// const result = R.set(fLens, upperName, person)
+
+const result = R.over(fLens, R.toUpper, person)
+
+console.log(result)
+document.getElementById('output').innerHTML = `${JSON.stringify(result)}`
---
+++ test.js
@@ -0,0 +1,70 @@
+const { match, countBy, identity, compose, toLower, invert, sortBy, map } = require('ramda');
+
+const persons = [
+    {
+        name: 'Vasa',
+        age: 25,
+        salary: 1500
+    },
+    {
+        name: 'Kola',
+        age: 27,
+        salary: 1400
+    },
+    {
+        name: 'Peta',
+        age: 45,
+        salary: 2200
+    }
+];
+
+const filteredPersons = persons
+    .filter((val) => val.age > 25)
+    .filter((val) => val.salary > 1500);
+
+// console.log(filteredPersons);
+
+const text = `
+'have no fear of this mess,'
+said the cat in the hat.
+'i always pick up all my playthings
+and so...
+i will show you another
+good trick that i know!'
+
+then we saw him pick up all the things that were down.
+he picked up the cake,
+and the rake, and the gown,
+and the milk, and the strings,
+and the books, and the dish,
+and the fan, and the cup,
+and the ship, and the fish.
+and he put them away.
+then he said, 'that is that.'
+and then he was gone
+with a tip of his hat.
+
+then our mother came in
+and she said to us two,
+'did you have any fun?
+tell me.  what did you do?'
+
+and sally and i did not know
+what to say.
+should we tell her
+the things that went on there that day?
+
+should we tell her about it?
+now, what SHOULD we do?
+well...
+what would YOU do
+if your mother asked YOU?`;
+
+const getCountWords = compose(
+    map(sortBy(identity)),
+    invert,
+    countBy(toLower),
+    match(/\w+/g)
+);
+
+console.log(getCountWords(text));
